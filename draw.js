c74cf3bef6a66458eeac332112acead456d6c5a4
Michael Leisch <michael.leisch@gmx.at>
2024-08-17 20:58:29 +0200

    main

---
+++ draw.js
@@ -0,0 +1,145 @@
+let menSingleDraw = [
+    [ // Round 1
+        {player1: "Michi", player2: "David L.", className: "r1"},
+        {player1: "Severin", player2: "Andi", className: "r2"},
+        {player1: "David K.", player2: "Jakob", className: "r3"},
+        {player1: "Lukas L.", player2: "Elias", className: "r4"},
+        {player1: "Thomas", player2: "Herbert", className: "r5"},
+        {player1: "Lukas P.", player2: "Julian", className: "r6"},
+        {player1: "Peter", player2: "Gerhard", className: "r7"},
+        {player1: "Peter S.", player2: "Patrick", className: "r8"},
+    ],
+    [ // Quarterfinal A
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "qA1"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "qA2"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "qA3"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "qA4"},
+    ],
+    [ // Semifinal A
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "sA1"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "sA2"},
+    ],
+    [ // Final A
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "fA"},
+    ],
+    [ // Winner A
+        {player1: "winnerA", result1: "6:4 6:4", className: "wA"},
+    ],
+    [ // Round 2 B
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "r2B1"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "r2B2"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "r2B3"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "r2B4"},
+    ],
+    [ // Round 3 B
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "qB1"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "qB2"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "qB3"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "qB4"},
+    ],
+    [ // Round 4 B
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "sB1"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "sB2"},
+    ],
+    [ // Round 5 B
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "fB"},
+    ],
+    [ // Winner B
+        {player1: "Mich", result1: "6:4 6:4", className: "wB"},
+    ],
+];
+
+let menDoubleDraw = [
+    [ // Round 1
+        {player1: "Lea / Elias", player2: "Bye", className: "r1"},
+        {player1: "Thomas / David", player2: "Peter / Paul", className: "r2"},
+        {player1: "Patrick Ö / Mario H", player2: "Patrick P / Mario V", className: "r3"},
+        {player1: "Bye", player2: "Herbert / Gerhard", className: "r4"},
+        {player1: "Peter / Andi", player2: "Bye", className: "r5"},
+        {player1: "Bye", player2: "David / Philipp", className: "r6"},
+        {player1: "Lukas / Jakob", player2: "Bye", className: "r7"},
+        {player1: "Bye", player2: "Michi / Luki", className: "r8"},
+    ],
+    [ // Quarterfinal A
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "qA1"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "qA2"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "qA3"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "qA4"},
+    ],
+    [ // Semifinal A
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "sA1"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "sA2"},
+    ],
+    [ // Final A
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "fA"},
+    ],
+    [ // Winner A
+        {player1: "Mich", result1: "6:4 6:4", className: "wA"},
+    ],
+    [ // Round 2 B
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "r2B1"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "r2B2"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "r2B3"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "r2B4"},
+    ],
+    [ // Round 3 B
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "qB1"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "qB2"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "qB3"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "qB4"},
+    ],
+    [ // Round 4 B
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "sB1"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "sB2"},
+    ],
+    [ // Round 5 B
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "fB"},
+    ],
+    [ // Winner B
+        {player1: "Mich", result1: "6:4 6:4", className: "wB"},
+    ],
+];
+
+let womenSingleDraw = [
+    [ // Round 1
+        {player1: "Helga", player2: "Andrea", className: "r1"},
+        {player1: "Edith", player2: "Karin", className: "r2"},
+        {player1: "Amelie", player2: "Marlene", className: "r3"},
+        {player1: "Rosa", player2: "Lea", className: "r4"}
+    ],
+    [ // Semifinal A
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "sA1"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "sA2"},
+    ],
+    [ // Final A
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "fA"},
+    ],
+    [ // Winner A
+        {player1: "Mich", result1: "6:4 6:4", className: "wA"},
+    ],
+    [ // Round 2 B
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "r2B1"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "r2B2"},
+    ],
+    [ // Semifinal B
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "sB1"},
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "sB2"},
+    ],
+    [ // Final B
+        {player1: "Mich", result1: "6:4 6:4", player2: "Yanik", result2: "6:4 6:4", className: "fB"},
+    ],
+    [ // Winner B
+        {player1: "Mich", result1: "6:4 6:4", className: "wB"},
+    ],
+];
+
+let womenDoublePlayer = [
+    "Karin / Rosa", "Amelie / Edith", "Marlene / Andrea", "Helga / Sabine",
+    "Karin / Rosa", "Amelie / Edith", "Marlene / Andrea", "Helga / Sabine",
+]
+let womenDoubleResults = [
+    "", "", "",
+    "", "", "",
+    "", "", "",
+    "", "", ""
+]
---
+++ script.js
@@ -0,0 +1,332 @@
+function getDraw(type, elem) {
+    if(elem){
+        document.querySelector("p.active").classList.remove("active");
+        elem.classList.add("active");
+    }
+
+    let draw
+
+    switch (type) {
+        case 'menSingle': draw = menSingleDraw; printDraw(16); break;
+        case 'menDouble': draw = menDoubleDraw; printDraw(16); break;
+        case 'womenSingle': draw = womenSingleDraw; printDraw(8); break;
+        case 'womenDouble': printDraw(4); womenDoubleDraw(); return;;
+    }
+
+
+    draw.forEach(round => {
+        round.forEach(match => {
+            let bracket = document.querySelector(`.${match.className}`);
+            if (bracket) {
+                let player1 = bracket.querySelectorAll("div.person")[0].querySelectorAll("p");
+                if(bracket.querySelectorAll("div.person")[1]) {
+                    let player2 = bracket.querySelectorAll("div.person")[1].querySelectorAll("p");
+                    player2[0].innerHTML = match.player2
+                    if(match.result2){
+                        player2[1].innerHTML = match.result2
+                    }
+                }
+
+                player1[0].innerHTML = match.player1
+
+                if(match.result1){
+                    player1[1].innerHTML = match.result1
+                }
+            }
+        });
+    });
+}
+
+function womenDoubleDraw(){
+    let nameFields =  document.querySelectorAll("table th:not(.black)")
+    for (let i = 0; i < nameFields.length; i++) {
+        nameFields[i].innerHTML = womenDoublePlayer[i]
+    }
+
+    let resultFields =  document.querySelectorAll("table td")
+    for (let i = 0; i < resultFields.length; i++) {
+        resultFields[i].innerHTML = womenDoubleResults[i]
+    }
+}
+
+function printDraw(type){
+
+    if(type === 16){
+        document.querySelector("main").innerHTML = `
+        <div class="left">
+            <div class="column" style="--top: 6.25%;">
+                <div class="bracket wB">
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                </div>
+            </div>
+
+            <div class="column" style="grid-template-rows: 1fr; --top: 6.25%;">
+                <div class="bracket fB">
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                </div>
+            </div>
+
+            <div class="column" style="grid-template-rows: 1fr 1fr; --top: 6.25%;">
+                <div class="bracket sB1">
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                </div>
+                <div class="bracket sB2">
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                </div>
+            </div>
+
+            <div class="column" style="grid-template-rows: repeat(4, 1fr); --top: 6.25%;">
+                <div class="bracket qB1">
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                </div>
+                <div class="bracket qB2">
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                </div>
+                <div class="bracket qB3">
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                </div>
+                <div class="bracket qB4">
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                </div>
+            </div>
+
+            <div class="column" style="grid-template-rows: 1fr 1fr 1fr 1fr;">
+                <div class="bracket r2B1">
+                    <div class="person"><p>naem</p><p>naem</p></div>
+                    <div class="person"><p>naem</p><p>naem</p></div>
+                </div>
+                <div class="bracket r2B2">
+                    <div class="person"><p>naem</p><p>naem</p></div>
+                    <div class="person"><p>naem</p><p>naem</p></div>
+                </div>
+                <div class="bracket r2B3">
+                    <div class="person"><p>naem</p><p>naem</p></div>
+                    <div class="person"><p>naem</p><p>naem</p></div>
+                </div>
+                <div class="bracket r2B4">
+                    <div class="person"><p>naem</p><p>naem</p></div>
+                    <div class="person"><p>naem</p><p>naem</p></div>
+                </div>
+            </div>
+        </div>
+        <div class="center column">
+            <div class="bracket r1">
+                <div class="person"><p>name</p><p></p></div>
+                <div class="person"><p>name</p><p></p></div>
+            </div>
+            <div class="bracket r2">
+                <div class="person"><p>name</p><p></p></div>
+                <div class="person"><p>name</p><p></p></div>
+            </div>
+            <div class="bracket r3">
+                <div class="person"><p>name</p><p></p></div>
+                <div class="person"><p>name</p><p></p></div>
+            </div>
+            <div class="bracket r4">
+                <div class="person"><p>name</p><p></p></div>
+                <div class="person"><p>name</p><p></p></div>
+            </div>
+            <div class="bracket r5">
+                <div class="person"><p>name</p><p></p></div>
+                <div class="person"><p>name</p><p></p></div>
+            </div>
+            <div class="bracket r6">
+                <div class="person"><p>name</p><p></p></div>
+                <div class="person"><p>name</p><p></p></div>
+            </div>
+            <div class="bracket r7">
+                <div class="person"><p>name</p><p></p></div>
+                <div class="person"><p>name</p><p></p></div>
+            </div>
+            <div class="bracket r8">
+                <div class="person"><p>name</p><p></p></div>
+                <div class="person"><p>name</p><p></p></div>
+            </div>
+        </div>
+        <div class="right">
+            <div class="column" style="grid-template-rows: 1fr 1fr 1fr 1fr;">
+                <div class="bracket qA1">
+                    <div class="person"><p>naem</p><p>naem</p></div>
+                    <div class="person"><p>naem</p><p>naem</p></div>
+                </div>
+                <div class="bracket qA2">
+                    <div class="person"><p>naem</p><p>naem</p></div>
+                    <div class="person"><p>naem</p><p>naem</p></div>
+                </div>
+                <div class="bracket qA3">
+                    <div class="person"><p>naem</p><p>naem</p></div>
+                    <div class="person"><p>naem</p><p>naem</p></div>
+                </div>
+                <div class="bracket qA4">
+                    <div class="person"><p>naem</p><p>naem</p></div>
+                    <div class="person"><p>naem</p><p>naem</p></div>
+                </div>
+            </div>
+
+            <div class="column" style="grid-template-rows: 1fr 1fr;">
+                <div class="bracket sA1">
+                    <div class="person"><p>mich</p><p>deimum</p></div>
+                    <div class="person"><p>mich</p><p>deimum</p></div>
+                </div>
+                <div class="bracket sA2">
+                    <div class="person"><p>peter</p><p>yanik</p></div>
+                    <div class="person"><p>peter</p><p>yanik</p></div>
+                </div>
+            </div>
+
+            <div class="column" style="grid-template-rows: 1fr;">
+                <div class="bracket fA">
+                    <div class="person"><p>mich</p><p>yanik</p></div>
+                    <div class="person"><p>mich</p><p>yanik</p></div>
+                </div>
+            </div>
+
+            <div class="column" style="--top: 6.25%;">
+                <div class="bracket wA">
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                </div>
+            </div>
+        </div>
+        `
+    }
+
+    if(type === 8){
+        document.querySelector("main").innerHTML = `
+        <div class="left">
+            <div class="column" style="--top: 12.5%;">
+                <div class="bracket wB">
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                </div>
+            </div>
+
+            <div class="column" style="grid-template-rows: 1fr; --top: 12.55%;">
+                <div class="bracket fB">
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                </div>
+            </div>
+
+            <div class="column" style="grid-template-rows: 1fr 1fr; --top: 12.50%;">
+                <div class="bracket sB1">
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                </div>
+                <div class="bracket sB2">
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                </div>
+            </div>
+
+            <div class="column" style="grid-template-rows: repeat(2, 1fr);">
+                <div class="bracket r2B1">
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                </div>
+                <div class="bracket r2B2">
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                </div>
+            </div>
+        </div>
+        <div class="center column" style="grid-template-rows: repeat(4, 1fr);">
+            <div class="bracket r1">
+                <div class="person"><p>name</p><p></p></div>
+                <div class="person"><p>name</p><p></p></div>
+            </div>
+            <div class="bracket r2">
+                <div class="person"><p>name</p><p></p></div>
+                <div class="person"><p>name</p><p></p></div>
+            </div>
+            <div class="bracket r3">
+                <div class="person"><p>name</p><p></p></div>
+                <div class="person"><p>name</p><p></p></div>
+            </div>
+            <div class="bracket r4">
+                <div class="person"><p>name</p><p></p></div>
+                <div class="person"><p>name</p><p></p></div>
+            </div>
+        </div>
+        <div class="right">
+            <div class="column" style="grid-template-rows: 1fr 1fr;">
+                <div class="bracket sA1">
+                    <div class="person"><p>mich</p><p>deimum</p></div>
+                    <div class="person"><p>mich</p><p>deimum</p></div>
+                </div>
+                <div class="bracket sA2">
+                    <div class="person"><p>peter</p><p>yanik</p></div>
+                    <div class="person"><p>peter</p><p>yanik</p></div>
+                </div>
+            </div>
+
+            <div class="column" style="grid-template-rows: 1fr;">
+                <div class="bracket fA">
+                    <div class="person"><p>mich</p><p>yanik</p></div>
+                    <div class="person"><p>mich</p><p>yanik</p></div>
+                </div>
+            </div>
+
+            <div class="column" style="--top: 6.25%;">
+                <div class="bracket wA">
+                    <div class="person"><p>mich</p><p>erg</p></div>
+                </div>
+            </div>
+        </div>
+        `
+    }
+
+    if(type === 4){
+        document.querySelector("main").innerHTML = `
+        <table>
+            <tr>
+                <th class="black"></th>
+                <th>name</th>
+                <th>name</th>
+                <th>name</th>
+                <th>name</th>
+            </tr>
+
+            <tr>
+                <th>name</th>
+                <th class="black"></th>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+
+            <tr>
+                <th>name</th>
+                <td></td>
+                <th class="black"></th>
+                <td></td>
+                <td></td>
+            </tr>
+
+            <tr>
+                <th>name</th>
+                <td></td>
+                <td></td>
+                <th class="black"></th>
+                <td></td>
+            </tr>
+            
+            <tr>
+                <th>name</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <th class="black"></th>
+            </tr>
+        </table>
+        `
+    }
+}
+
+
+getDraw("menSingle")
