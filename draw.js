83851dda57f95be3c17892b1ce532bb0aea7346d
Michael Leisch <michael.leisch@gmx.at>
2024-08-24 21:28:50 +0200

    a

--- draw.js
+++ draw.js
@@ -10,7 +10,7 @@ let menSingleDraw = [
         {player1: "Peter S.", player2: "Patrick", className: "r8"},
     ],
     [ // Quarterfinal A
-        {player1: "ㅤ", result1: "26.08. 18:00", player2: "ㅤ", result2: "24.08. 18:00", className: "qA1"},
+        {player1: "ㅤ", result1: "26.08. 18:00", player2: "Severin", result2: "6:1 6:2", className: "qA1"},
         {player1: "Jakob", result1: "6:0 7:6", player2: "", result2: "", className: "qA2"},
         {player1: "ㅤ", result1: "28.08. 17:00", player2: "", result2: "", className: "qA3"},
         {player1: "Peter P.", result1: "6:2 6:2", player2: "Patrick", result2: "6:2 6:0", className: "qA4"},
@@ -26,7 +26,7 @@ let menSingleDraw = [
         {player1: "", result1: "", className: "wA"},
     ],
     [ // Round 2 B
-        {player1: "", result1: "", player2: "", result2: "", className: "r2B1"},
+        {player1: "", result1: "", player2: "Andi", result2: "", className: "r2B1"},
         {player1: "David K.", result1: "", player2: "", result2: "", className: "r2B2"},
         {player1: "", result1: "", player2: "", result2: "", className: "r2B3"},
         {player1: "Gerhard", result1: "", player2: "Peter S.", result2: "", className: "r2B4"},
