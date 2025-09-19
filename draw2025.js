6114203b0de271ad6fcea2761d6ea6346fa5aaf7
Michael Leisch <michael.leisch@gmx.at>
2025-09-19 19:21:12 +0200

    .

--- draw2025.js
+++ draw2025.js
@@ -22,6 +22,9 @@ const menSingleDraw = new Tournament("Herren Einzel", [
     new Round("Final A", [
         new Match("Michi", "", "6:1 6:1", "", "fA"),
     ]),
+    new Round("Winner A", [
+       new Match("", "", "21.09. 12:00", "", "wA")
+    ]),
     new Round("Round 2 B", [
         new Match("Bye", "Consti", "", "", "r2B1"),
         new Match("Colin", "David", "", "", "r2B2"),
@@ -35,8 +38,8 @@ const menSingleDraw = new Tournament("Herren Einzel", [
         new Match("Mario", "Simon", "", "(2)", "qB4"),
     ]),
     new Round("Semifinal B", [
-        new Match("", "", "", "", "sB1"),
-        new Match("", "Simon", "", "6:1 6:2", "sB2"),
+        new Match("Jakob", "Andreas", "6:2 6:4", "w. o.", "sB1"),
+        new Match("Peter P.", "Simon", "6:0 6:1", "6:1 6:2", "sB2"),
     ]),
     new Round("Semifinal C", [
         new Match("Bye", "Colin", "(5)", "(6)", "rC1"),
@@ -44,6 +47,9 @@ const menSingleDraw = new Tournament("Herren Einzel", [
     ]),
     new Round("Final C", [
         new Match("Colin", "Harald", "", "", "fC"),
+    ]),
+    new Round("Winner C", [
+        new Match("Harald", "", "6:3 6:2", "", "wC")
     ])
 ]);
 
@@ -60,6 +66,9 @@ const womenSingleDraw = new Tournament("Damen Einzel", [
     ]),
     new Round("Final A", [
        new Match("Helga", "Amelie", "6:0 6:0", "6:2 6:0", "fA"),
+    ]),
+    new Round("Winner A", [
+       new Match("", "", "21.09. 14:00", "", "wA")
     ]),
     new Round("Round 2 B", [
         new Match("Bye", "Marlene", "", "", "r2B1"),
@@ -89,15 +98,21 @@ const menDoubleDraw = new Tournament("Herren Doppel", [
         new Match("Patrick / Mario O.", "Thomas / David", "6:3 6:2", "6:1 6:2", "sA2"),
     ]),
     new Round("Final A", [
-        new Match("Michi / Simon", "", "6:4 6:4", "", "fA"),
+        new Match("Michi / Simon", "Patrick / Mario O.", "6:4 6:4", "4:6 7:5 7:6", "fA"),
+    ]),
+    new Round("Winner A", [
+        new Match("", "", "21.09. 10:00", "", "wA")
     ]),
     new Round("Round 2 B", [
         new Match("Herbert / Gerhard", "Daniel / Philipp", "", "", "r2B1"),
         new Match("Jakob / Mario V.", "Colin / Consti", "", "", "r2B2"),
     ]),
     new Round("Semifinal B", [
-        new Match("Herbert / Gerhard", "", "6:0 6:0", "(2)", "sB1"),
-        new Match("", "Peter / Paul", "", "(1)", "sB2"),
+        new Match("Herbert / Gerhard", "Thomas / David", "6:0 6:0", "(2)", "sB1"),
+        new Match("Jakob / Mario V.", "Peter / Paul", "7:6 0:6 6:4", "(1)", "sB2"),
+    ]),
+    new Round("Final B", [
+        new Match("Thomas / David", "", "6:3 6:1", "", "fB")
     ])
 ])
 
@@ -109,24 +124,24 @@ const womenDoubleDraw = new Tournament("Damen Doppel", [
     ]),
     new Round("Round Robin 2", [
         new Match("Amelie / Edith", "Helga / Marlene", "4:6 4:6", "", "rr1"),
-        new Match("Amelie / Edith", "Karin / Emma", "", "", "rr3"),
-        new Match("Amelie / Edith", "Andrea / Erna", "", "", "rr4"),
+        new Match("Amelie / Edith", "Karin / Emma", "6:3 7:6", "", "rr3"),
+        new Match("Amelie / Edith", "Andrea / Erna", "6:2 6:1", "", "rr4"),
     ]),
     new Round("Round Robin 3", [
         new Match("Karin / Emma", "Helga / Marlene", "4:6 4:6", "", "rr1"),
-        new Match("Karin / Emma", "Amelie / Edith", "", "", "rr2"),
+        new Match("Karin / Emma", "Amelie / Edith", "3:6 6:7", "", "rr2"),
         new Match("Karin / Emma", "Andrea / Erna", "6:4 6:2", "", "rr3"),
     ]),
     new Round("Round Robin 4", [
         new Match("Andrea / Erna", "Helga / Marlene", "0:6 1:6", "", "rr1"),
-        new Match("Andrea / Erna", "Amelie / Edith", "", "", "rr2"),
+        new Match("Andrea / Erna", "Amelie / Edith", "2:6 1:6", "", "rr2"),
         new Match("Andrea / Erna", "Karin / Emma", "4:6 2:6", "", "rr3"),
     ]),
 ]);
 
 const roundRobinData = [
     { name: "Helga / Marlene", spiele: 3, siege: 3, saetze: "6:0", games: "36:17" },
-    { name: "Karin / Emma", spiele: 2, siege: 1, saetze: "2:2", games: "20:18" },
-    { name: "Amelie / Edith", spiele: 1, siege: 0, saetze: "0:2", games: "8:12" },
-    { name: "Andrea / Erna", spiele: 2, siege: 0, saetze: "0:4", games: "7:24" }
+    { name: "Amelie / Edith", spiele: 3, siege: 2, saetze: "4:2", games: "33:27" },
+    { name: "Karin / Emma", spiele: 3, siege: 1, saetze: "2:2", games: "29:30" },
+    { name: "Andrea / Erna", spiele: 3, siege: 0, saetze: "0:6", games: "10:36" }
 ];
