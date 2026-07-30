8506debcfd36f4bc0f5325dfb6de7421f90935db
Michael Leisch <92372034+Michiii11@users.noreply.github.com>
2026-07-30 13:09:36 +0200

    .

--- draw2026.js
+++ draw2026.js
@@ -6,18 +6,18 @@ const menSingleDraw = new Tournament("Herren Einzel", [
         new Match("Bye", "David L.", "", "", "r4"),
         new Match("Jakob", "Bye", "", "", "r5"),
         new Match("Bye", "Lukas", "", "", "r6"),
-        new Match("Martin Moser", "Simon", "", "", "r7"),
+        new Match("Martin Moser", "Bye", "", "", "r7"),
         new Match("Bye", "Peter", "", "", "r8"),
     ]),
     new Round("Quarterfinal A", [
         new Match("Thomas", "Colin", "", "", "qA1"),
         new Match("", "David L.", "", "", "qA2"),
         new Match("Jakob", "Lukas", "", "", "qA3"),
-        new Match("", "Peter", "", "", "qA4"),
+        new Match("Martin", "Peter", "", "", "qA4"),
     ]),
     new Round("Semifinal A", [
         new Match("Thomas", "", "6:1 6:1", "", "sA1"),
-        new Match("Jakob", "", "6:1 6:2", "", "sA2"),
+        new Match("Jakob", "Peter", "6:1 6:2", "6:1 6:1", "sA2"),
     ]),
     new Round("Round 2 B", [
         new Match("Bye", "Bye", "", "", "r2B1"),
@@ -27,7 +27,7 @@ const menSingleDraw = new Tournament("Herren Einzel", [
     ]),
     new Round("Quarterfinal B", [
         new Match("Bye", "Lukas", "", "(3)", "qB1"),
-        new Match("", "", "", "(4)", "qB2"),
+        new Match("", "Martin", "", "(4)", "qB2"),
         new Match("Bye", "Colin", "", "(1)", "qB3"),
         new Match("", "", "", "(2)", "qB4"),
     ]),
@@ -67,15 +67,15 @@ const menDoubleDraw = new Tournament("Herren Doppel", [
         new Match("Thomas / David L.", "Harald / Julian", "", "", "r1"),
         new Match("David K. / Jakob", "Mario O. / Patrick Ö.", "", "", "r2"),
         new Match("Mario V. / Patrick P.", "Philipp / Colin", "", "", "r3"),
-        new Match("Simon / Peter", "Herbert / Gerhard", "", "", "r4"),
+        new Match("Bye", "Herbert / Gerhard", "", "", "r4"),
     ]),
     new Round("Semifinal A", [
         new Match("Thomas / David L.", "Mario O. / Patrick Ö.", "6:3 6:3", "6:2 6:1", "sA1"),
-        new Match("", "", "", "", "sA2"),
+        new Match("", "Herbert / Gerhard", "", "", "sA2"),
     ]),
     new Round("Round 2 B", [
         new Match("Harald / Julian", "David K. / Jakob", "", "", "r2B1"),
-        new Match("", "", "", "", "r2B2"),
+        new Match("", "Bye", "", "", "r2B2"),
     ]),
     new Round("Semifinal B", [
         new Match("", "", "", "(2)", "sB1"),
