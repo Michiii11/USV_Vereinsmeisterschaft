09c49e7a48ca4f1be65da4b7af8140e380a60f6e
Michael Leisch <92372034+Michiii11@users.noreply.github.com>
2026-08-06 19:17:53 +0200

    .

--- draw2026.js
+++ draw2026.js
@@ -77,15 +77,15 @@ const menDoubleDraw = new Tournament("Herren Doppel", [
     ]),
     new Round("Semifinal A", [
         new Match("Thomas / David L.", "Mario O. / Patrick Ö.", "6:3 6:3", "6:2 6:1", "sA1"),
-        new Match("", "Herbert / Gerhard", "", "", "sA2"),
+        new Match("Philipp / Colin", "Herbert / Gerhard", "6:0 3:6 10:6", "", "sA2"),
     ]),
     new Round("Round 2 B", [
         new Match("Harald / Julian", "David K. / Jakob", "", "", "r2B1"),
-        new Match("", "Bye", "", "", "r2B2"),
+        new Match("Mario V. / Patrick P.", "Bye", "", "", "r2B2"),
     ]),
     new Round("Semifinal B", [
         new Match("", "", "", "(2)", "sB1"),
-        new Match("", "", "", "(1)", "sB2"),
+        new Match("Mario V. / Patrick P.", "", "", "(1)", "sB2"),
     ])
 ])
 
