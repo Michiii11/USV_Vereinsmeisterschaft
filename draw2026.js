8175ba6a036258bc8e75c4dea1f92e9482846248
Michael Leisch <92372034+Michiii11@users.noreply.github.com>
2026-08-25 13:57:18 -0400

    .

--- draw2026.js
+++ draw2026.js
@@ -121,7 +121,7 @@ const womenDoubleDraw = new Tournament("Damen Doppel", [
         new Match("Andrea / Helga", "Rosa / Marlene", "", "", "rr5"),
         new Match("Amelie / Edith", "Sophie / Valentina", "6:1 6:2", "", "rr3"),
         new Match("Amelie / Edith", "Emma / Karin", "2:6 6:4 12:10", "", "rr4"),
-        new Match("Amelie / Edith", "Rosa / Marlene", "5:7 2:6", "", "rr5"),
+        new Match("Amelie / Edith", "Rosa / Marlene", "7:5 6:2", "", "rr5"),
         new Match("Sophie / Valentina", "Emma / Karin", "4:6 3:6", "", "rr4"),
         new Match("Sophie / Valentina", "Rosa / Marlene", "5:7 6:3 9:11", "", "rr5"),
         new Match("Emma / Karin", "Rosa / Marlene", "6:2 6:2", "", "rr5"),
@@ -129,9 +129,9 @@ const womenDoubleDraw = new Tournament("Damen Doppel", [
 ]);
 
 const roundRobinData = [
+    { name: "Amelie / Edith", spiele: 3, siege: 3, saetze: "6:1", games: "45:30" },
     { name: "Andrea / Helga", spiele: 2, siege: 2, saetze: "4:0", games: "24:7" },
-    { name: "Amelie / Edith", spiele: 3, siege: 2, saetze: "4:2", games: "27:26" },
-    { name: "Rosa / Marlene", spiele: 3, siege: 2, saetze: "4:2", games: "27:30" },
-    { name: "Emma / Karin", spiele: 4, siege: 2, saetze: "4:4", games: "36:31" },
-    { name: "Sophie / Valentina", spiele: 4, siege: 0, saetze: "1:8", games: "26:46" },
+    { name: "Emma / Karin", spiele: 4, siege: 2, saetze: "5:4", games: "46:43" },
+    { name: "Rosa / Marlene", spiele: 3, siege: 2, saetze: "2:5", games: "32:45" },
+    { name: "Sophie / Valentina", spiele: 4, siege: 0, saetze: "1:8", games: "35:57" },
 ];
